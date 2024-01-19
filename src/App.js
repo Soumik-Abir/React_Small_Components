2f9a97159528d82b4dec45afaa0fd1dfe6ae908e
Soumik Mukherjee <mukherjeesoumik700@gmail.com>
2024-01-20 02:43:50 +0530

    Add new components and hooks

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import "./App.css";
 import Accordian from "./components/accordian";
 import ModalTest from "./components/custom-modal-popup/modal-test";
 import TabTest from "./components/custom-tabs/tab-test";
+import FeatureFlags from "./components/feature-flag";
+import FeatureFlagGlobalState from "./components/feature-flag/context";
+import GithubProfileFinder from "./components/github-profile-finder";
 import ImageSlider from "./components/image-slider";
 import LightDarkMode from "./components/light-dark-mode";
 import LoadMoreData from "./components/load-more-data";
 import QRCodeGenerator from "./components/qr-code-generator";
 import RandomColor from "./components/random-color";
 import ScrollIndicator from "./components/scroll-indicator";
+import ScrollToTopAndBottom from "./components/scroll-to-top-and-bottom";
+import ScrollToSection from "./components/scroll-to-top-and-bottom/scroll-to-section";
+import SearchAutocomplete from "./components/search-autocomplete-with-api";
 import StarRating from "./components/star-rating";
+import TicTacToe from "./components/tic-tac-toe";
 import TreeView from "./components/tree-view";
 import menus from "./components/tree-view/data";
+import UseFetchHookTest from "./components/use-fetch/test";
+import UseOnclickOutsideTest from "./components/use-outside-click/test";
+import UseWindowResizeTest from "./components/use-window-resize/test";
 
 function App() {
   return (
@@ -52,6 +62,35 @@ function App() {
 
       {/* Custom Modal Component */}
       <ModalTest/>
+
+      {/* Github profile finder */}
+      <GithubProfileFinder/>
+
+      {/* Search Autocomplete */}
+      <SearchAutocomplete/>
+
+      {/* Tic tac toe */}
+      <TicTacToe/>
+
+      {/* Feature Flag IMplementation */}
+      {/* <FeatureFlagGlobalState>
+        <FeatureFlags />
+      </FeatureFlagGlobalState> */}
+
+      {/* useFetch - Custom Hook Test*/}
+      <UseFetchHookTest/>
+
+      {/* Use Onclick Outside Hook Test */}
+      <UseOnclickOutsideTest/>
+
+      {/* Use Window Resize Hook Test */}
+      <UseWindowResizeTest/>
+
+      {/* Scroll to Top and Bottom */}
+      {/* <ScrollToTopAndBottom/> */}
+
+      {/* Scroll to a Particular Section */}
+      {/* <ScrollToSection/> */}
     </div>
   );
 }
