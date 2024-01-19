3291b047fc8a7b319526c560de4e7ae73df5f9ad
Soumik Mukherjee <mukherjeesoumik700@gmail.com>
2024-01-19 23:21:16 +0530

    Add new components and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Accordian from "./components/accordian";
+import ModalTest from "./components/custom-modal-popup/modal-test";
+import TabTest from "./components/custom-tabs/tab-test";
 import ImageSlider from "./components/image-slider";
 import LightDarkMode from "./components/light-dark-mode";
 import LoadMoreData from "./components/load-more-data";
 import QRCodeGenerator from "./components/qr-code-generator";
 import RandomColor from "./components/random-color";
+import ScrollIndicator from "./components/scroll-indicator";
 import StarRating from "./components/star-rating";
 import TreeView from "./components/tree-view";
 import menus from "./components/tree-view/data";
@@ -40,6 +43,15 @@ function App() {
 
       {/* light and dark theme switch */}
       <LightDarkMode/>
+
+      {/* Scroll indicator component */}
+      <ScrollIndicator url={"https://dummyjson.com/products?limit=10"} />
+
+      {/* Custom tabs component */}
+      <TabTest />
+
+      {/* Custom Modal Component */}
+      <ModalTest/>
     </div>
   );
 }
